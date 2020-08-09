4c9ca0cdc0dc498413ce85422414e156e53c3bf2
Biswajit Rout <biswajit.rout2000@gmail.com>
2020-08-09 13:30:59 +0000

    update dburl again

--- app.js
+++ app.js
@@ -15,9 +15,8 @@ var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
-
-mongoose.connect(process.env.DATABASEURL, {
-//mongoose.connect('mongodb+srv://Biswajit:biswajit@1612@cluster0.3tnbh.mongodb.net/yelp_camp?retryWrites=true&w=majority', {
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp"
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
